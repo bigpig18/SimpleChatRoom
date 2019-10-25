bc008ca0aaa7e6592d2f677f51e4610611fe85b9
liyun <2726829719@qq.com>
2019-10-25 17:06:22 +0800

    "nio模型 - 服务端 - 常量定义"

--- nio-chatroom/src/constant/ChatConstant.java
+++ nio-chatroom/src/constant/ChatConstant.java
@@ -7,6 +7,6 @@ package constant;
 public class ChatConstant {
     public final static String DEFAULT_HOST = "127.0.0.1";
     public final static int DEFAULT_PORT = 8081;
-    public final static String EXIT = "EXIT";
+    public final static String EXIT = "exit";
     public static final int BUFFER_SIZE = 1024;
 }
